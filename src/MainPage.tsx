34f51fb353a610650ce30eba5a4ddd5c2fe4caed
2salted <noamgraf123@gmail.com>
2024-03-21 15:13:32 -0100

    added inputs and label and css styles

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -2,7 +2,7 @@ import Calculator from "./components/Calculator";
 
 export default function MainPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center w-screen h-screen">
       <Calculator />
     </div>
   );
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,7 +1,10 @@
 export default function Calculator() {
   return (
-    <div className="flex flex-row w-1/2">
-      <div className="flex-1">s</div>
+    <div className="flex flex-row w-3/5">
+      <div className="flex flex-1 justify-center flex-col">
+        <label className="font-bold text-sm pb-2">Loan amount</label>
+        <input className="border rounded-md p-2 pl-2 outline-none border-gray-400"></input>
+      </div>
       <div className="flex-auto">d</div>
     </div>
   );
