3343056818c73a3b3f7c29fb32c60d5befc5c1fb
2salted <noamgraf123@gmail.com>
2024-03-21 18:28:04 -0100

    finished main css style for first input

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,13 +1,18 @@
 export default function Calculator() {
   return (
-    <div className="flex flex-row w-3/5 border border-gray-400 rounded-md p-4">
-      <div className="flex-1 justify-center">
+    <div className="flex flex-row w-3/5 border border-gray-400">
+      <div className="w-1/2 justify-center bg-gray-100 p-4">
         <div className="flex flex-col">
           <label className="font-bold text-sm pb-2">Loan amount</label>
           <input className="border rounded-md p-2 outline-none border-gray-400"></input>
         </div>
       </div>
-      <div className="flex-auto">z</div>
+      <div className="w-1/2 pl-4">
+        <div className="flex flex-col">
+          <div className="text-center">Monthly Payments</div>
+          <div>123</div>
+        </div>
+      </div>
     </div>
   );
 }
