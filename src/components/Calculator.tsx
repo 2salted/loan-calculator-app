f0469d5c377846bad56758a93d6df9efc868c2f2
2salted <noamgraf123@gmail.com>
2024-03-22 10:59:06 -0100

    updated css UI of calculator component

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,16 +1,36 @@
 export default function Calculator() {
   return (
-    <div className="flex flex-row w-3/5 border border-gray-400">
+    <div className="flex flex-row w-4/5 border border-gray-400">
       <div className="w-1/2 justify-center bg-gray-100 p-4">
         <div className="flex flex-col">
-          <label className="font-bold text-sm pb-2">Loan amount</label>
-          <input className="border rounded-md p-2 outline-none border-gray-400"></input>
+          <label htmlFor="input1" className="font-bold text-sm pb-2">
+            Loan amount
+          </label>
+          <input
+            id="input1"
+            className="border rounded-md p-2 outline-none border-gray-400"
+          ></input>
+          <label htmlFor="input1" className="font-bold text-sm pb-2 pt-4">
+            Loan term in years
+          </label>
+          <input
+            id="input1"
+            className="border rounded-md p-2 outline-none border-gray-400"
+          ></input>
         </div>
       </div>
-      <div className="w-1/2 pl-4">
+      <div className="w-1/2 p-4">
         <div className="flex flex-col">
-          <div className="text-center">Monthly Payments</div>
-          <div>123</div>
+          <div className="text-center pb-3">Monthly Payments</div>
+          <div className="flex flex-row justify-center">
+            <span className="font-bold">$</span>
+            <div className="text-center font-black text-5xl pb-4">93.2</div>
+          </div>
+          <div className="flex flex-row justify-between py-2">
+            <div className="font-medium text-sm">Total principal paid:</div>
+            <div className="font-medium text-sm">$5000</div>
+          </div>
+          <hr />
         </div>
       </div>
     </div>
