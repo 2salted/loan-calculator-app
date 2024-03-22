b2705045779dc767ecc68322fa41fd7547cc357a
2salted <noamgraf123@gmail.com>
2024-03-22 20:09:48 -0100

    update css gui for mobile

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,7 +1,7 @@
 export default function Calculator() {
   return (
-    <div className="flex flex-row lg:w-4/5 w-5/6 border border-gray-400">
-      <div className="w-1/2 justify-center bg-gray-100 p-4">
+    <div className="flex md:flex-row flex-col lg:w-4/6 w-5/6 border border-gray-400">
+      <div className="md:w-1/2 w-full justify-center bg-gray-100 p-4">
         <div className="flex flex-col">
           <label htmlFor="input1" className="font-bold text-sm pb-2">
             Loan amount
@@ -24,10 +24,12 @@ export default function Calculator() {
             id="input3"
             className="border rounded-md p-2 outline-none border-gray-400"
           ></input>
-          <button className="bg-[#0055ff] rounded-md text-white px-4">Calculate</button>
+          <div className="flex pt-4 justify-center md:justify-start">
+            <button className="bg-[#0055ff] rounded-md text-white py-2 px-5">Calculate</button>
+          </div>
         </div>
       </div>
-      <div className="w-1/2 p-4">
+      <div className="md:w-1/2 w-full p-4">
         <div className="flex flex-col">
           <div className="text-center pb-3">Monthly Payments</div>
           <div className="flex flex-row justify-center">
