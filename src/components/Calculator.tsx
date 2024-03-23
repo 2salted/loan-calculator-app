5a6fc23dcc2aefdab0748240ba9b3d2558eca057
2salted <noamgraf123@gmail.com>
2024-03-22 23:13:04 -0100

    update to the calculator functionality

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export default function Calculator() {
+  const [loanAmount, setLoanAmount] = useState<string>("");
+  const [loanTerm, setLoanTerm] = useState<string>("");
+  const [interestRate, setInterestRate] = useState<string>("");
+
   return (
     <div className="flex md:flex-row flex-col lg:w-4/6 w-5/6 border border-gray-400">
       <div className="md:w-1/2 w-full justify-center bg-gray-100 p-4">
@@ -8,7 +14,11 @@ export default function Calculator() {
           </label>
           <input
             id="input1"
+            value={loanAmount}
             className="border rounded-md p-2 outline-none border-gray-400"
+            onChange={(e) => {
+              setLoanAmount(e.target.value);
+            }}
           ></input>
           <label htmlFor="input2" className="font-bold text-sm pb-2 pt-4">
             Loan term in years
@@ -16,16 +26,39 @@ export default function Calculator() {
           <input
             id="input2"
             className="border rounded-md p-2 outline-none border-gray-400"
+            value={loanTerm}
+            onChange={(e) => {
+              setLoanTerm(e.target.value);
+            }}
           ></input>
           <label htmlFor="input3" className="font-bold text-sm pb-2 pt-4">
-            Interest per year
+            Interest rate per year
           </label>
           <input
             id="input3"
+            value={interestRate}
             className="border rounded-md p-2 outline-none border-gray-400"
+            onChange={(e) => {
+              setInterestRate(e.target.value);
+            }}
           ></input>
           <div className="flex pt-4 justify-center md:justify-start">
-            <button className="bg-[#0055ff] rounded-md text-white py-2 px-5">Calculate</button>
+            <button
+              type="button"
+              className="bg-[#0055ff] rounded-md text-white py-2 px-5"
+              onClick={() => {
+                if (loanAmount && loanTerm && interestRate) {
+                  let percentage = parseFloat(interestRate) / 100;
+                  console.log("percentage: " + percentage);
+                  let yearlyPayment = parseFloat(loanAmount) * percentage;
+                  console.log("yearly payment: " + yearlyPayment);
+                  let monthlyPayment = yearlyPayment / 12;
+                  console.log("yearly payment: " + monthlyPayment);
+                }
+              }}
+            >
+              Calculate
+            </button>
           </div>
         </div>
       </div>
